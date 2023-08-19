cf8183f74afda99d709f1efc674c126cd7f96ca2
Anna Drobot <adrobo@softserveinc.com>
2023-08-19 18:20:55 +0300

    UC#10 Implement regex

--- RegexUseCase/RegexUseCase/Program.cs
+++ RegexUseCase/RegexUseCase/Program.cs
@@ -1,10 +1,24 @@
-﻿namespace RegexUseCase
+﻿using System.Text.RegularExpressions;
+
+namespace RegexUseCase
 {
     public static class Program
     {
         public static void Main(string[] args)
         {
-            ;
+            var input = @"Ab!1bc";
+            var maxLength = 10U;
+            var result = IsMatch(maxLength, input);
+            Console.WriteLine($"input: {input}");
+            Console.WriteLine($"maxLength: {maxLength}");
+            Console.WriteLine($"result: {result}");
+        }
+
+        public static bool IsMatch(uint maxLength, string input)
+        {
+            var pattern = @"^(?=.{1," + maxLength + @"}$)(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!\x22#\$%&'\(\)\*\+,-\./:;<=>\?@\[\\\]\^_`{\|}~])([A-Za-z\d]|[!\x22#\$%&'\(\)\*\+,-\./:;<=>\?@\[\\\]\^_`{\|}~])*$";
+            var regex = new Regex(pattern);
+            return regex.IsMatch(input);
         }
     }
 }
